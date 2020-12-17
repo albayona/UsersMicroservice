f65bf2342f62c2814fef3b5b2503c09ffce07d22
Andres Bayona <al.bayona@uniandes.edu.co>
2020-12-17 17:04:10 -0500

    NA

--- contentApp/contentApi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContentapiConfig(AppConfig):
-    name = 'contentApi'
--- searchApp/interests/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class interestsConfig(AppConfig):
-    name = 'interests'
